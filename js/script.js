40a5232ac4054973ad76743262732a669f83861a
AlexBieg <alexbieg95@gmail.com>
2016-01-18 19:27:04 -0800

    added current post

--- js/script.js
+++ js/script.js
@@ -55,11 +55,10 @@ app.controller("PortfolioController", function($scope, $http) {
 
 app.controller("BlogController", function($scope, $http) {
 	$http.get('blog/blog.csv').then( function(data) {
-		console.log(data);
 		$scope.blogInfo = Papa.parse(data.data, {header: true}).data;
 
 		$http.get($scope.blogInfo[0].url).then(function(post) {
-			console.log(post)
+			$scope.currPost = post.data;
 		});
 	});
 });
