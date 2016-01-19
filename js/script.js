20999d936543f24c31286a94fd18e36157e272ee
AlexBieg <alexbieg95@gmail.com>
2016-01-18 19:13:40 -0800

    even more

--- js/script.js
+++ js/script.js
@@ -55,6 +55,7 @@ app.controller("PortfolioController", function($scope, $http) {
 
 app.controller("BlogController", function($scope, $http) {
 	$http.get('blog/blog.csv').then( function(data) {
+		console.log(data);
 		$scope.blogInfo = Papa.parse(data.data, {header: true}).data;
 
 		console.log($scope.blogInfo);
