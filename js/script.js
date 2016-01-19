5719b0336e21a5f66a5e54fbcc7dabe125feda66
AlexBieg <alexbieg95@gmail.com>
2016-01-18 22:46:09 -0800

    moasdf

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		controller : 'PortfolioController',
 	});
 	$stateProvider.state('blog', {
-		url : '/blog',
+		url : '/blog/',
 		templateUrl : 'templates/blog.html',
 		controller : 'BlogController',
 	});
