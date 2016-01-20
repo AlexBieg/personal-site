52942d1a837d08f55e7a2c9b24f4c89a6aa37e23
AlexBieg <alexbieg95@gmail.com>
2016-01-19 20:12:33 -0800

    asdfasdf

--- posts/get-posts.php
+++ posts/get-posts.php
@@ -2,9 +2,9 @@
 	error_reporting(E_ALL); ini_set('display_errors', '1');
 	$posts = glob("*.html");
 	$json = [];
-	// foreach ($posts as $post) {
-	// 	$file = ["title" => $post, "content" => file_get_contents($post), "data" => date ("F d Y", filemtime($post))]
-	// 	array_push($json, $file);
-	// }
+	foreach ($posts as $post) {
+		$file = ["title" => $post, "content" => file_get_contents($post), "data" => filemtime($post)]
+		array_push($json, $file);
+	}
 	echo json_encode($json);
  ?>
