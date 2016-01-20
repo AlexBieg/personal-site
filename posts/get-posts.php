e658891dfdbb8f89eeb01ea4372d9db3e553f38d
AlexBieg <alexbieg95@gmail.com>
2016-01-19 20:14:26 -0800

    asdfasd

--- posts/get-posts.php
+++ posts/get-posts.php
@@ -3,7 +3,7 @@
 	$posts = glob("*.html");
 	$json = [];
 	foreach ($posts as $post) {
-		$file = ["title" => $post, "content" => file_get_contents($post), "data" => filemtime($post)]
+		$file = ["title" => $post, "content" => file_get_contents($post), "data" => filemtime($post)];
 		array_push($json, $file);
 	}
 	echo json_encode($json);
