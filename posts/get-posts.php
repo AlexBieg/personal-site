cf43311a2d212599e57fc9d8cb5a75faa4b9be56
AlexBieg <alexbieg95@gmail.com>
2016-01-19 20:04:23 -0800

    fixed error

--- posts/get-posts.php
+++ posts/get-posts.php
@@ -2,7 +2,7 @@
 	$posts = glob("*.html");
 	$json = [];
 	foreach ($posts as $post) {
-		$file = ["title" => $post, "content" => file_get_contents($post), "data" => date ("F d Y", filemtime($post)]
+		$file = ["title" => $post, "content" => file_get_contents($post), "data" => date ("F d Y", filemtime($post))]
 		array_push($json, $file);
 	}
 	echo json_encode($json);
