18f6151cfc7d044fecee3bf1d7c6c4a103a0c948
Alex Bieg <Alex Bieg>
2017-03-18 12:17:17 -0700

    renamed index.js to server.js for arvixe

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/app/index.html'));
 });
 
-app.listen(8080, function() {
-    console.log("Listening on port 8080.");
+app.listen(3000, function() {
+    console.log("Listening on port 3000.");
 });
