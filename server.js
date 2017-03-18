1aa576b535358ff0a4112935787f19961b279a0e
Alex Bieg <Alex Bieg>
2017-03-18 13:29:43 -0700

    added procfile

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/app/index.html'));
 });
 
-app.listen(3000, function() {
-    console.log("Listening on port 3000.");
+app.listen(app.get('port'), function() {
+    console.log("Listening on port ", app.get('port'));
 });
