b9605b57834f37a7105817bbaa86750e42860ea4
Alex Bieg <Alex Bieg>
2017-03-18 13:59:07 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
-var path = require('path');
 
 app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname+"/app");
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/app/index.html'));
+    res.render("index.html");
 });
 
 app.listen(app.get('port'), function() {
